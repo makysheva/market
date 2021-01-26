d1fdf71c5d524302b09d66318a43d47874db8f4d
makysheva <makysheva.aliya@gmail.com>
2021-01-26 22:24:13 +0600

    исправление адаптивности

--- src/blocks/header/menu/menu.js
+++ src/blocks/header/menu/menu.js
@@ -1,1 +1,11 @@
 import "../../../index";
+
+const burgerMenuBtn = document.querySelector('.header__burger')
+const menuListUl = document.querySelector('.menu__list--mobile')
+
+const handleBurgerClick = () => {
+    menuListUl.classList.toggle('active')
+    burgerMenuBtn.classList.toggle('active-color')
+}
+
+burgerMenuBtn.addEventListener('click', handleBurgerClick)
